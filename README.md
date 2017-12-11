b0f4946e2cbc48de00e3c1ef03b3c49f3cd2ed2e
Varija Agarwal <agarwalvarija@gmail.com>
2017-12-10 19:24:43 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Memory Game Project
+
+
+## Table of Contents
+
+* [Description](#instructions)
+* [Contributing](#contributing)
+
+## Description
+
+This is a simple memory game app that I created as a part of my Udacity Front End Web Development Nanodegree.
+
+## Contributing
+
+Not accepting contributions at this time.
---
+++ js/app.js
@@ -0,0 +1,83 @@
+//Create a list of 16 cards
+  let cards = ['&spades;', '&spades;', '&clubs;', '&clubs;', '&hearts;', '&hearts;', '&diams;', '&diams;', '&#9728;', '&#9728;', '&#9729;', '&#9729;', ' &#9731;', ' &#9731;', '&#9730;', '&#9730;'];
+
+  //Be able to shuffle that list
+  // Shuffle function from http://stackoverflow.com/a/2450976
+  function shuffle(array) {
+      var currentIndex = array.length, temporaryValue, randomIndex;
+
+      while (currentIndex !== 0) {
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex -= 1;
+          temporaryValue = array[currentIndex];
+          array[currentIndex] = array[randomIndex];
+          array[randomIndex] = temporaryValue;
+      }
+
+      return array;
+  }
+
+  // Display the list: loop through each card and create its HTML, and add the HTML to the page
+    cards = shuffle(cards);
+    let openArray = [];
+    let tableHTML = '';
+    let arrInd = 0;
+    for (let row=1; row<=4; row++) {
+      tableHTML += '<tr>';
+      for (let col=1; col<=4; col++) {
+        tableHTML += `<td class="closed">${cards[arrInd]}</td>`;
+        arrInd++;
+      }
+      tableHTML += '</tr>';
+    }
+    let playTable = $('table');
+    playTable.html(tableHTML);
+
+    /* Keep track of:
+      Moves - number of clicks/card flips
+      Matches - number of matched pairs so far
+    */
+    let moves = 0;
+    let matches = 0;
+
+    // set event listener on click for table
+      /* When you click on a card:
+          1. Add 1 to moves and update moves display
+          2. Display its symbol
+          3. Add it to a list of open cards
+          4. If the list of open cards is not empty, check if the new card matches any of them.
+            If cards match, lock the cards in open position. Matches++
+            If cards do not match, remove them from the list of open cards and flip them over (no longer display their symbol).
+          5. If matches===8 then display win message.
+      */
+    playTable.on('click', 'td', function(e) {
+      e.preventDefault();
+      moves++;
+      $('#moveCount').html(`Moves: ${moves}`);
+      $(this).toggleClass('closed');
+      openArray.push($(e.target));
+      openArray = newCardAdded(openArray);
+      if (matches===8) {
+        let winString = `<div class="container"><h1>Congrats! You won in ${moves} moves!</h1></div>`;
+        $('body').html(winString);
+      }
+    });
+
+    function newCardAdded(openArray) {
+      if (openArray.length==2) {
+        if (openArray[0].text()===openArray[1].text()) {
+          // if match, then just remove from openArray and do nothing (keep them open)
+          matches++;
+        } else {
+          setTimeout(function (openArray) {
+            openArray[0].toggleClass('closed');
+            openArray[1].toggleClass('closed');
+          }, 500, openArray);
+        }
+        openArray = [];
+      }
+      return openArray;
+    }
+
+
+  // add restart button that starts a new game
